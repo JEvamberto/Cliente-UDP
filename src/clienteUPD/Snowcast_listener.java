da80ac7705b97efa66db3df505debb5b5dd07d27
José Evamberto <evamberto.lima@gmail.com>
2018-08-23 21:53:30 -0300

    Escutar

---
+++ src/clienteUPD/Snowcast_listener.java
@@ -0,0 +1,77 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clienteUPD;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.ByteArrayInputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.SocketException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
+
+/**
+ *
+ * @author jose
+ */
+public class Snowcast_listener{
+
+    //Comando Hello
+    byte commandType = 0;
+    short updPort;
+    //SetStation
+    byte commandType1 = 1;
+    short stationNumber;
+    
+
+    //receptor
+    public static void main(String[] args) {
+        try {
+            
+
+            int pacote = 50000;
+        
+            DatagramSocket receber = new DatagramSocket(12344);
+
+            byte[] myBuffer = new byte[pacote];
+
+            DatagramPacket pkgRecebe = new DatagramPacket(myBuffer, myBuffer.length);
+            
+            //new Player();
+                while(true){
+                    receber.receive(pkgRecebe);
+                byte audio[] = pkgRecebe.getData();
+                
+                    Player toca= new Player ( new ByteArrayInputStream(audio));
+                    toca.play();
+                   
+                }
+            
+            
+            
+            
+         
+        } catch (SocketException ex) {
+            Logger.getLogger(Snowcast_listener.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(Snowcast_listener.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (JavaLayerException ex) {
+            Logger.getLogger(Snowcast_listener.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+
+
+}
